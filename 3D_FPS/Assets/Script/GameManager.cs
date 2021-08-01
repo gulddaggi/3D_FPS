36985c02d271c8ca22cacaf7ae57876b9f05f0b7
gulddaggi <81425764+gulddaggi@users.noreply.github.com>
2021-08-01 19:45:39 +0900

    Water

--- 3D_FPS/Assets/Script/GameManager.cs
+++ 3D_FPS/Assets/Script/GameManager.cs
@@ -8,6 +8,9 @@ public class GameManager : MonoBehaviour
     public static bool isOpenInventory = false; //인벤토리 활성화
     public static bool isOpenCraftManual = false; //건축 메뉴창 활성화
 
+    public static bool isNight = false;
+    public static bool isWater = false;
+
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked; //커서 잠금
